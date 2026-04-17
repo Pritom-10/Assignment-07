bbfde9c2354349c2303a0fa36751706f03cc25a2
Pritom-10 <pritomshiro@gmail.com>
2026-04-17 10:54:53 +0600

    navbar and homepage added

--- src/Pages/Stats/Stats.jsx
+++ src/Pages/Stats/Stats.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import { Pie, PieChart } from "recharts";
+import { Legend, Pie, PieChart } from "recharts";
 const Stats = () => {
   const data = [
-    { name: "Call", value: 400, fill: "#0088FE" },
-    { name: "Text", value: 300, fill: "#00C49F" },
-    { name: "Video", value: 300, fill: "#FFBB28" },
+   
   ];
   return (
     <div>
@@ -21,14 +19,15 @@ const Stats = () => {
           data={data}
           innerRadius="80%"
           outerRadius="100%"
-          // Corner radius is the rounded edge of each pie slice
+          
           cornerRadius="50%"
           fill="#8884d8"
-          // padding angle is the gap between each pie slice
+        
           paddingAngle={5}
           dataKey="value"
           isAnimationActive={true}
         />
+        <Legend />
       </PieChart>
     </div>
   );
